7a64bb6ab7eb002e7ed6dad5cbfa80798ea0a5dc
jagdeep2393 <jagdeep10790@gmail.com>
2024-04-09 17:08:39 +0530

    Test

--- resources/views/agent/index.blade.php
+++ resources/views/agent/index.blade.php
@@ -223,27 +223,42 @@
                 <div class="col-md-6 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <p class="card-title">Order Details</p>
-                            <p class="font-weight-500">The total number of sessions within the date range. It is the period time a user is actively engaged with your website, page or app, etc</p>
-                            <div class="d-flex flex-wrap mb-5">
-                                <div class="mr-5 mt-3">
-                                    <p class="text-muted">Order value</p>
+                            <div class="d-flex flex-wrap d-flex justify-content-between mb-5">
+                                <div class="mt-1">
+                                    <p class="text-muted">Last Login</p>
                                     <h3 class="text-primary fs-30 font-weight-medium">12.3k</h3>
                                 </div>
-                                <div class="mr-5 mt-3">
-                                    <p class="text-muted">Orders</p>
+                                <div class="mt-1">
+                                    <p class="text-muted">Last Logout</p>
                                     <h3 class="text-primary fs-30 font-weight-medium">14k</h3>
                                 </div>
-                                <div class="mr-5 mt-3">
-                                    <p class="text-muted">Users</p>
+                                <div class="mt-1">
+                                    <p class="text-muted">Last Break</p>
                                     <h3 class="text-primary fs-30 font-weight-medium">71.56%</h3>
                                 </div>
-                                <div class="mt-3">
-                                    <p class="text-muted">Downloads</p>
-                                    <h3 class="text-primary fs-30 font-weight-medium">34040</h3>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex flex-wrap d-flex justify-content-between mb-5">
+                                <div class="card mt-1">
+                                    <div class="card-body rounded bg-success">
+                                        <p class="text-muted">Last Login</p>
+                                        <h3 class="text-primary fs-30 font-weight-medium">12.3k</h3>
+                                    </div>
+                                </div>
+                                <div class="card mt-1">
+                                    <div class="card-body rounded bg-success">
+                                        <p class="text-muted">Last Logout</p>
+                                        <h3 class="text-primary fs-30 font-weight-medium">14k</h3>
+                                    </div>
+                                </div>
+                                <div class="card mt-1">
+                                    <div class="card-body rounded bg-success">
+                                        <p class="text-muted">Last Break</p>
+                                        <h3 class="text-primary fs-30 font-weight-medium">71.56%</h3>
+                                    </div>
                                 </div>
                             </div>
-                            <canvas id="order-chart"></canvas>
                         </div>
                     </div>
                 </div>
